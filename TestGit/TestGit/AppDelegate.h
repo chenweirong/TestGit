63c81892317b463b2ddab2a0bc30666cf335abee
陈洧荣 <chenweirong@chenweirongdeMacBook-Air.local>
2017-09-27 14:40:01 +0800

    Create

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by 陈洧荣 on 2017/9/27.
+//  Copyright © 2017年 陈洧荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by 陈洧荣 on 2017/9/27.
+//  Copyright © 2017年 陈洧荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
